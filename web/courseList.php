5d9d64cb72ad0b6f5342d01ecc170fb6f6149614
Andrew Rivett <veggiefrog@gmail.com>
2019-02-27 13:24:30 -0500

    Added instructor to course listings

--- web/courseList.php
+++ web/courseList.php
@@ -11,6 +11,40 @@
 //
 function ciniki_courses_web_courseList($ciniki, $settings, $tnid, $type, $when) {
 
+    //
+    // Get the course instructors
+    //
+    $strsql = "SELECT offerings.id, " 
+        . "instructors.id AS instructor_id, "
+        . "instructors.first, "
+        . "instructors.last "
+        . "FROM ciniki_course_offerings AS offerings "
+        . "LEFT JOIN ciniki_course_offering_instructors AS oi ON ("
+            . "offerings.id = oi.offering_id "
+            . "AND oi.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+            . ") "
+        . "LEFT JOIN ciniki_course_instructors AS instructors ON ("
+            . "oi.instructor_id = instructors.id "
+            . "AND instructors.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+            . ") "
+        . "WHERE offerings.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+        . "AND offerings.status = 10 "
+        . "AND (offerings.webflags&0x01) = 0 "    // Visible online
+        . "ORDER BY offerings.id, instructors.first, instructors.last "
+        . "";
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryIDTree');
+    $rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.courses', array(
+        array('container'=>'offerings', 'fname'=>'id', 'fields'=>array('id')),
+        array('container'=>'instructors', 'fname'=>'instructor_id', 'fields'=>array('id'=>'instructor_id', 'first', 'last')),
+        ));
+    if( $rc['stat'] != 'ok' ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.courses.113', 'msg'=>'Unable to load offerings', 'err'=>$rc['err']));
+    }
+    $instructors = isset($rc['offerings']) ? $rc['offerings'] : array();
+
+    //
+    // Load the course listings
+    //
     $strsql = "SELECT ciniki_course_offerings.id, "
         . "ciniki_course_offerings.course_id, "
         . "ciniki_course_offerings.permalink, "
@@ -82,6 +116,17 @@ function ciniki_courses_web_courseList($ciniki, $settings, $tnid, $type, $when) 
 
     foreach($rc['categories'] as $cid => $cat) {
         foreach($cat['list'] as $lid => $item) {
+            if( isset($instructors[$item['id']]['instructors']) ) {
+                $names = '';
+                foreach($instructors[$item['id']]['instructors'] as $instructor) {
+                    $names .= ($names != '' ? ', ' : '') . $instructor['first'] . ' ' . $instructor['last'];
+                }
+                if( count($instructors[$item['id']]['instructors']) == 1 ) {
+                    $rc['categories'][$cid]['list'][$lid]['subtitle2'] = 'Instructor: ' . $names;
+                } elseif( count($instructors[$item['id']]['instructors']) > 1 ) {
+                    $rc['categories'][$cid]['list'][$lid]['subtitle2'] = 'Instructors: ' . $names;
+                }
+            }
             if( ciniki_core_checkModuleFlags($ciniki, 'ciniki.courses', 0x01) && $item['code'] != '' ) {
                 $rc['categories'][$cid]['list'][$lid]['title'] = $item['code'] . ' - ' . $item['course_name'];
             } elseif( ciniki_core_checkModuleFlags($ciniki, 'ciniki.courses', 0x20) && $item['offering_code'] != '' ) {
