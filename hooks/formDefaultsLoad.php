358326b47d069de7b7ac0090ec217b60bd817faf
Andrew Rivett <veggiefrog@gmail.com>
2022-05-05 11:27:54 -0400

    Added instructor form defaults

--- hooks/formDefaultsLoad.php
+++ hooks/formDefaultsLoad.php
@@ -18,41 +18,60 @@ function ciniki_courses_hooks_formDefaultsLoad(&$ciniki, $tnid, $args) {
         return array('stat'=>'ok');
     }
     $form = $args['form'];
-   
+  
     //
     // Process the customer if specified
     //
     if( isset($form['customer_id']) && $form['customer_id'] != '' && $form['customer_id'] > 0 ) {
-
         //
-        // FIXME: Link instructors to customers module
+        // Check for instructor details
         //
+        $strsql = "SELECT instructors.id, "
+            . "instructors.primary_image_id, "
+            . "instructors.short_bio, "
+            . "instructors.full_bio, "
+            . "instructors.url "
+            . "FROM ciniki_course_instructors AS instructors "
+            . "WHERE instructors.customer_id = '" . ciniki_core_dbQuote($ciniki, $form['customer_id']) . "' "
+            . "AND instructors.tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+            . "";
+        $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.courses', 'instructor');
+        if( $rc['stat'] != 'ok' ) {
+            return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.courses.226', 'msg'=>'Unable to load instructor', 'err'=>$rc['err']));
+        }
+        if( isset($rc['instructor']) ) {
+            $instructor = $rc['instructor'];
 
-        //
-        // Load the instructor if they exist
-        //
+            //
+            // Load the refs
+            //
+            ciniki_core_loadMethod($ciniki, 'ciniki', 'courses', 'hooks', 'formFieldRefs');
+            $rc = ciniki_courses_hooks_formFieldRefs($ciniki, $tnid, array());
+            if( $rc['stat'] != 'ok' ) {
+                return $rc;
+            }
+            $refs = $rc['refs'];        
 
-
-/*
-        if( isset($form['sections']) ) {
-            foreach($form['sections'] as $sid => $section) {
-                if( isset($section['fields']) ) {
-                    foreach($section['fields'] as $fid => $field) {
-                        //
-                        // Check if field ref is for customer and if the reference exists
-                        //
-                        if( isset($field['field_ref']) && $field['field_ref'] != '' 
-                            && preg_match("/^ciniki\.customers\.customer\.([^\.]+)/", $field['field_ref'], $m)
-                            && isset($refs[$field['field_ref']])
-                            ) {
-                            if( isset($customer[$m[1]]) ) {
-                                $form['sections'][$sid]['fields'][$fid]['default'] = $customer[$m[1]];
+            if( isset($form['sections']) ) {
+                foreach($form['sections'] as $sid => $section) {
+                    if( isset($section['fields']) ) {
+                        foreach($section['fields'] as $fid => $field) {
+                            //
+                            // Check if field ref is for instructor and if the reference exists
+                            //
+                            if( isset($field['field_ref']) && $field['field_ref'] != '' 
+                                && preg_match("/^ciniki\.courses\.instructor\.([^\.]+)/", $field['field_ref'], $m)
+                                && isset($refs[$field['field_ref']])
+                                ) {
+                                if( isset($instructor[$m[1]]) ) {
+                                    $form['sections'][$sid]['fields'][$fid]['default'] = $instructor[$m[1]];
+                                }
                             }
                         }
                     }
                 }
-            }
-        }  */
+            } 
+        }
     }
 
     return array('stat'=>'ok', 'form'=>$form);
